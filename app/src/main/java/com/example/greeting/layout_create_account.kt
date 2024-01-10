f70699a70c55f965f8bac517ca913cb304565ec8
namaek_2 <science4588@gmail.com>
2024-01-10 17:18:48 +0900

    create account 생성

---
+++ app/src/main/java/com/example/greeting/layout_create_account.kt
@@ -0,0 +1,11 @@
+package com.example.greeting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_create_account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_create_account)
+    }
+}
