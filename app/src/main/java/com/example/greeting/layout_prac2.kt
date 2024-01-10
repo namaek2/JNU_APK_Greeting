a3844d1724956450c989ad45a508ad6f471a4a89
namaek_2 <science4588@gmail.com>
2024-01-10 15:16:33 +0900

    Layout 생성 및 실행2 수정

---
+++ app/src/main/java/com/example/greeting/layout_prac2.kt
@@ -0,0 +1,11 @@
+package com.example.greeting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_prac2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_prac2)
+    }
+}
--- app/src/main/java/com/example/greeting/layout_prac3.kt
+++ app/src/main/java/com/example/greeting/layout_prac3.kt
@@ -3,9 +3,9 @@ package com.example.greeting
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class layout_prac3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_layout_prac3)
     }
 }
